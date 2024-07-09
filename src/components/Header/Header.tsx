43e6eb314a54e9ab11b34c21399496e1a0e85be3
Леонид <lenya.zverevich@gmail.com>
2024-07-09 11:09:20 +0300

    added header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,18 @@
-import s from './Header.module.scss'
+import { Link } from 'react-router-dom'
 import headerLogo from '../../assets/logo.png'
+import s from './Header.module.scss'
 export const Header = () => {
   return (
-    <header>
+    <header className={s.header}>
       <div className={s.headerRow}>
         <div className="container">
           {/*     Могу совмещать модули и простой css? Просто контейнер везде одинаковый, в каждый модуль не хочу писать  */}
-          <div className={s.headerLogo}>
-            <img src={headerLogo} />
-            <span>ClosedCritic</span>
-          </div>
+          <Link className={s.headerLink} to={'/'}>
+            <div className={s.headerLogo}>
+              <img src={headerLogo} />
+              <span>ClosedCritic</span>
+            </div>
+          </Link>
         </div>
       </div>
     </header>
