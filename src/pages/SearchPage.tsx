c9cd6ba69e4df633f97c36ac6f77ac5461bd3aae
Леонид <lenya.zverevich@gmail.com>
2024-07-13 21:38:20 +0300

    fixed hook usage

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -2,13 +2,18 @@ import { useSearchParams } from 'react-router-dom'
 import { Header } from '../components/Header/Header'
 import { gameApi } from '../redux/services/gameService'
 import { SearchList } from '../components/SearchList/SearchList'
+import { useEffect, useState } from 'react'
 
 export const SearchPage = () => {
   const [searchParams] = useSearchParams()
-  const queryParams = searchParams.get('query')
-  const { data } = queryParams
-    ? gameApi.useGetGameSearchQuery(queryParams)
-    : { data: undefined }
+  const query = searchParams.get('query')
+  const [queryParams, setQueryParams] = useState<string>('')
+
+  useEffect(() => {
+    setQueryParams(query!)
+  }, [searchParams, query])
+
+  const { data } = gameApi.useGetGameSearchQuery(queryParams)
 
   return (
     <>
