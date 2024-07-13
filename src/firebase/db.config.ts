d71f446617a87800728d489982d2e8c9a4c05ca5
Леонид <lenya.zverevich@gmail.com>
2024-07-13 20:24:23 +0300

    replace the database on firestore

--- src/firebase/db.config.ts
+++ src/firebase/db.config.ts
@@ -1,6 +1,6 @@
 import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
-import { getDatabase } from 'firebase/database'
+import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -13,4 +13,4 @@ const firebaseConfig = {
 
 export const app = initializeApp(firebaseConfig)
 export const auth = getAuth(app)
-export const database = getDatabase(app)
+export const db = getFirestore(app)
