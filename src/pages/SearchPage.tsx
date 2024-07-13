da8ba24a1ba6cca363972bcf29d4ab70085094c0
Леонид <lenya.zverevich@gmail.com>
2024-07-13 20:31:39 +0300

    deleted unused code

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -9,58 +9,7 @@ export const SearchPage = () => {
   const { data } = queryParams
     ? gameApi.useGetGameSearchQuery(queryParams)
     : { data: undefined }
-  // const data: RawSearchedGames[] = [
-  //   {
-  //     id: 194,
-  //     name: 'Minecraft',
-  //     dist: 0,
-  //   },
-  //   {
-  //     id: 14349,
-  //     name: 'Minecraft Legends',
-  //     dist: 0.44444442,
-  //   },
-  //   {
-  //     id: 9258,
-  //     name: 'Minecraft Dungeons',
-  //     dist: 0.4736842,
-  //   },
-  //   {
-  //     id: 2094,
-  //     name: 'Minecraft: Wii U Edition',
-  //     dist: 0.5833334,
-  //   },
-  //   {
-  //     id: 238,
-  //     name: 'MouseCraft',
-  //     dist: 0.6,
-  //   },
-  //   {
-  //     id: 15664,
-  //     name: 'Minecraft x Dungeons & Dragons.',
-  //     dist: 0.6153846,
-  //   },
-  //   {
-  //     id: 480,
-  //     name: 'Minecraft: Xbox One Edition',
-  //     dist: 0.6296296,
-  //   },
-  //   {
-  //     id: 2255,
-  //     name: 'Minecraft: Story Mode - Season One',
-  //     dist: 0.6666666,
-  //   },
-  //   {
-  //     id: 5372,
-  //     name: 'Minecraft: Story Mode - Season Two',
-  //     dist: 0.6666666,
-  //   },
-  //   {
-  //     id: 11777,
-  //     name: 'Minecraft Dungeons: Hidden Depths',
-  //     dist: 0.6875,
-  //   },
-  // ]
+
   return (
     <>
       <Header />
@@ -68,5 +17,3 @@ export const SearchPage = () => {
     </>
   )
 }
-
-// ОДИНАКОВЫЕ УРЛЫ НЕ ЗАХОДЯТ
