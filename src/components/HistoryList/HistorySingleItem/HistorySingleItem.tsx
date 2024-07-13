15401a1929bb519c2dce68d418be23db81dd1c35
Леонид <lenya.zverevich@gmail.com>
2024-07-13 20:20:22 +0300

    added history page

---
+++ src/components/HistoryList/HistorySingleItem/HistorySingleItem.tsx
@@ -0,0 +1,27 @@
+import { Link } from 'react-router-dom'
+import { deleteUrlHistory } from '../../../redux/store/historySlice/historySlice'
+import { useAppDispatch } from '../../../hooks/reduxHooks'
+import s from './HistorySingleItem.module.scss'
+
+interface HistorySingleItemProps {
+  url: string
+  id: string
+}
+
+export const HistorySingleItem = ({ url, id }: HistorySingleItemProps) => {
+  const dispatch = useAppDispatch()
+
+  return (
+    <div className={s.link}>
+      <Link to={`/search?query=${url}`} className={s.url}>
+        {url}
+      </Link>
+      <span
+        onClick={() => dispatch(deleteUrlHistory({ url, id }))}
+        className={s.deletebtn}
+      >
+        x
+      </span>
+    </div>
+  )
+}
