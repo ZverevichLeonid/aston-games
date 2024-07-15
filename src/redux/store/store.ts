814502671a84c188fe95f69e9e32f4a142aff995
Леонид <lenya.zverevich@gmail.com>
2024-07-15 15:30:11 +0300

    added favorite slice and fix types

--- src/redux/store/store.ts
+++ src/redux/store/store.ts
@@ -2,11 +2,12 @@ import { configureStore } from '@reduxjs/toolkit'
 import { gameApi } from '../services/gameService'
 import { reducer as userReducer } from '../store/userSlice/userSlice'
 import { reducer as historyReducer } from '../store/historySlice/historySlice'
-
+import { reducer as favoritesReducer } from '../store/favoritesSlice/favoritesSlice'
 export const store = configureStore({
   reducer: {
     user: userReducer,
     history: historyReducer,
+    favorites: favoritesReducer,
     [gameApi.reducerPath]: gameApi.reducer,
   },
   middleware: getDefaultMiddleware =>
