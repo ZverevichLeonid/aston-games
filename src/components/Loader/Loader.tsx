df5082218f75e6a28c54cf67c343ce5e5529579c
Леонид <lenya.zverevich@gmail.com>
2024-07-15 15:31:08 +0300

    added loader component

---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,9 @@
+import loader from '../../assets/loader.gif'
+import s from './Loader.module.scss'
+export const Loader = () => {
+  return (
+    <div className={s.loader}>
+      <img src={loader} alt="loader" />
+    </div>
+  )
+}
