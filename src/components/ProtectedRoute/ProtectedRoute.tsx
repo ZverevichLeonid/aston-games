2926ea6bcadae101bee79d7d88af0cf1fea0d208
Леонид <lenya.zverevich@gmail.com>
2024-07-18 14:33:05 +0300

    added propTypes

--- src/components/ProtectedRoute/ProtectedRoute.tsx
+++ src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -1,5 +1,5 @@
 import { Navigate, Outlet } from 'react-router-dom'
-
+import PropTypes from 'prop-types'
 interface ProtectedRouteProps {
   isAllowed: boolean
 }
@@ -9,4 +9,7 @@ export const ProtectedRoute = ({ isAllowed }: ProtectedRouteProps) => {
     return <Navigate to={'/signin'} replace />
   }
   return <Outlet />
+}
+ProtectedRoute.propTypes = {
+  isAllowed: PropTypes.bool.isRequired,
 }
